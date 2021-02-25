62c29ea4018e9276b4c900013e7aaa75c97e240b
Richard <richardyjkim@gmail.com>
2021-02-25 13:03:49 -0800

    droppable function added, jqeruy mobile access ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,7 +70,7 @@ $(".list-group").on("blur", "textarea", function(){
 
   // get the task's position in the list of other li element
   let index = $(this)
-  .closest(".list-group-itme")
+  .closest(".list-group-item")
   .index();
 
   tasks[status][index].text = text;
@@ -171,6 +171,70 @@ $("#task-form-modal .btn-primary").click(function() {
     saveTasks();
   }
 });
+
+// sortable
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    // console.log("activate", this);
+  },
+  deactivate: function(event) {
+    // console.log("deactivate", this);
+  },
+  over: function(event) {
+    // console.log("over",event.target);
+  },
+  out: function(event) {
+    // console.log("out", event.target);
+  },
+  update: function(event) {
+    let tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function(){
+      let text = $(this)
+      .find("p")
+      .text()
+      .trim();
+
+      let date = $(this)
+      .find("span")
+      .text()
+      .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    let arrName = $(this)
+    .attr("id")
+    .replace("list-","");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+// droppable
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out)");
+  }
+})
 
 // remove all tasks
 $("#remove-tasks").on("click", function() {
