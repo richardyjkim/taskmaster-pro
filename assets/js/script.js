9c3a2cfa824258a46c55081ccab6267b53069030
Richard <richardyjkim@gmail.com>
2021-02-25 22:27:11 -0800

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,6 +168,12 @@ let auditTask = function(taskEl) {
   }
 };
 
+setInterval(function(){
+  $(".card .list-group-item").each(function(index, el){
+    auditTask(el);  
+  });
+}, (1000*60)*30);
+
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function() {
   // clear values
@@ -181,7 +187,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -209,16 +215,25 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+
     // console.log("activate", this);
   },
   deactivate: function(event) {
     // console.log("deactivate", this);
+    $(this).removeClass("dropover bottom-trash bottom-trash-drag");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
     // console.log("over",event.target);
+    $(event.target).addClass("dropover-active");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event) {
     // console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   },
   update: function(event) {
     let tempArr = [];
